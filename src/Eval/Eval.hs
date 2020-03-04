runeksvendsen/rule-lang

commit baeec34990ef80ed623ab83cb5729016bbbb6aee

Increase separation of position & group comparisons

src/Eval/Eval.hs
@@ -84,21 +84,18 @@ evalComparison
     :: Comparison
     -> ScopeData
     -> EvalM (ComparisonResult Position)
-evalComparison (Comparison valueExpr bCompare value) scopeData =
-    let fCompare = comparator bCompare in
-    case valueExpr of
-        GroupValueExpr groupValueExpr ->
-            evalComparisonGroup groupValueExpr fCompare value scopeData
-        PosValueExpr posValueExpr ->
-            evalComparisonPos posValueExpr fCompare value (currentLevelPos scopeData)
+evalComparison (GroupComparison groupValueExpr bCompare groupValue) scopeData =
+    evalComparisonGroup groupValueExpr (comparator bCompare) groupValue scopeData
+evalComparison (PosComparison fieldName bCompare fieldValue) scopeData =
+    evalComparisonPos fieldName (comparator bCompare) fieldValue (currentLevelPos scopeData)
 
 evalComparisonGroup
     :: GroupValueExpr
-    -> (Value -> Value -> Bool)
-    -> Value
+    -> (GroupValue -> GroupValue -> Bool)
+    -> GroupValue
     -> ScopeData
     -> EvalM (ComparisonResult Position)
-evalComparisonGroup groupValueExpr fCompare value scopeData = do
+evalComparisonGroup groupValueExpr fCompare expectedValue scopeData = do
     case groupValueExpr of
         CountDistinct fieldName -> do
             count <- evalCountDistinct currentLevelPositions fieldName
@@ -109,27 +106,25 @@ evalComparisonGroup groupValueExpr fCompare value scopeData = do
   where
     currentLevelPositions = currentLevelPos scopeData
     groupCompare calculatedValue positions = do
-        if fCompare calculatedValue value
+        if fCompare expectedValue calculatedValue
             then ComparisonResult (Just positions) Nothing
             else ComparisonResult Nothing (Just positions)
 
 evalComparisonPos
-    :: PosValueExpr
-    -> (t -> Value -> Bool)
-    -> t
+    :: FieldName
+    -> (FieldValue -> FieldValue -> Bool)
+    -> FieldValue
     -> NonEmpty Position
     -> EvalM (ComparisonResult Position)
-evalComparisonPos posValueExpr fCompare value currentLevelPositions = do
-    case posValueExpr of
-        Get fieldName -> do
-            valueFields <- lookupFields fieldName currentLevelPositions
-            return $ addManyResults (fCompare value) (NE.map (fmap Field) valueFields)
+evalComparisonPos fieldName fCompare expectedValue currentLevelPositions = do
+    valueFields <- lookupFields fieldName currentLevelPositions
+    return $ addManyResults (fCompare expectedValue) valueFields
   where
-    addManyResults :: (Value -> Bool) -> NonEmpty (Position, Value) -> ComparisonResult Position
+    addManyResults :: (FieldValue -> Bool) -> NonEmpty (Position, FieldValue) -> ComparisonResult Position
     addManyResults f =
         foldr testAdd (ComparisonResult Nothing Nothing)
       where
-        testAdd :: (Position, Value) -> ComparisonResult Position -> ComparisonResult Position
+        testAdd :: (Position, FieldValue) -> ComparisonResult Position -> ComparisonResult Position
         testAdd (item, testVal) cr =
             if f testVal
                 then cr { compareTrue  = consMaybeNE item (compareTrue cr) }
@@ -144,7 +139,7 @@ evalSumOver
     :: ScopeData
     -> FieldName
     -> Maybe GroupName
-    -> EvalM (NonEmpty Position, Value)
+    -> EvalM (NonEmpty Position, GroupValue)
 evalSumOver scopeData fieldName groupNameOpt = do
     (positions, sumValue) <- evalSum fieldName (currentLevelPos scopeData)
     addPositions positions =<< case groupNameOpt of
@@ -176,7 +171,7 @@ evalSum fieldName positions = do
 evalCountDistinct
     :: NonEmpty Position    -- ^ Current level positions
     -> FieldName
-    -> EvalM Value
+    -> EvalM GroupValue
 evalCountDistinct currentLevelPositions fieldName = do
     newGrouping <- mkCurrentLevelGroupingM fieldName currentLevelPositions
     let result = fromIntegral $ length $ M.keys newGrouping

src/Absyn.hs
@@ -1,11 +1,10 @@
+{-# OPTIONS_GHC -fno-warn-orphans #-}
 module Absyn
 ( -- * Abstract syntax
   RuleExpr(..)
 , Comparison(..)
-, ValueExpr(..)
 , GroupValueExpr(..)
-, PosValueExpr(..)
-, Value(..)
+, GroupValue(..)
   -- * Value types
 , FieldName
 , FieldValue
@@ -18,26 +17,17 @@ where
 import LangPrelude
 import Comparison                           as Comparison
 import qualified Data.Aeson                 as Json
-import           Data.String                (IsString(..))
 
 
 type FieldName = Text
 type FieldValue = Json.Value
 type GroupName = Text
 
-data Value =
+data GroupValue =
       Count Word
     | Sum Double
-    | Field FieldValue
     | Percent Double
         deriving Show
-
--- A value for a position
-data PosValueExpr =
-      -- Violator: (FieldName, Position)
-      -- "InstrumentType"
-      Get FieldName
-        deriving (Eq, Show)
 
 -- A value for a group
 data GroupValueExpr =
@@ -50,11 +40,6 @@ data GroupValueExpr =
     | SumOver FieldName (Maybe GroupName)
         deriving (Eq, Show)
 
-data ValueExpr =
-      GroupValueExpr GroupValueExpr
-    | PosValueExpr PosValueExpr
-        deriving (Eq, Show)
-
 -- Examples:
 --     *Input*                          *ValueExpr*                       *Compare*       *Value*
 --  |------------|----------------------------------------------------|---------------|-------------|
@@ -65,7 +50,9 @@ data ValueExpr =
 --     grouping     sum     Value           (relative to Portfolio)     >               5%
 --     grouping     sum     Value           (relative to Country)       <               20%
 data Comparison =
-      Comparison ValueExpr BoolCompare Value
+      GroupComparison GroupValueExpr BoolCompare GroupValue
+    | PosComparison FieldName BoolCompare FieldValue
+        deriving Show
 
 data RuleExpr
     -- Two RuleExpr in the same context.
@@ -178,35 +165,29 @@ data RuleExpr
 
 {- #### TYPE CLASS INSTANCES #### -}
 
-instance IsString Value where
-    fromString str = Field $ Json.String (toS str)
-
 -- TODO: static check of invalid comparisons
-instance Eq Value where
+instance Eq GroupValue where
     (Count   a) == (Count   b) = a == b
     (Sum     a) == (Sum     b) = a == b
     (Percent a) == (Percent b) = a == b
-    (Field (Json.String strA)) == (Field (Json.String strB)) =
-        strA == strB
-    -- Below throws an error
-    (Field a) == (Field b) =
-        error $ "Field comparison not implemented: " ++ show (a,b)
     (==) a b = error $ "Invalid comparison: " ++ show (a, b)
 
+-- instance Eq GroupValue where
+--     (Field (Json.String strA)) == (Field (Json.String strB)) =
+--         strA == strB
+--     -- Below throws an error
+--     (Field a) == (Field b) =
+--         error $ "Field comparison not implemented: " ++ show (a,b)
+--     (==) a b = error $ "Invalid comparison: " ++ show (a, b)
+
 -- TODO: static check of invalid comparisons
-instance Ord Value where
+instance Ord GroupValue where
     (Count   a) `compare` (Count   b) = a `compare` b
     (Sum     a) `compare` (Sum     b) = a `compare` b
     (Percent a) `compare` (Percent b) = a `compare` b
-    -- Below throws an error
-    (Field (Json.String strA)) `compare` (Field (Json.String strB)) =
-        strA `compare` strB
-    -- Below throws an error
-    (Field a) `compare` (Field b) =
-        error $ "Field comparison not implemented: " ++ show (a,b)
     compare a b = error $ "Invalid comparison: " ++ show (a, b)
 
--- TMP:
-instance Show Comparison where
-    show (Comparison valueExpr _ value) =
-        "(Comparison (" ++ show valueExpr ++ ") (" ++ show value ++ "))"
+instance Ord Json.Value where
+    (Json.Number a) `compare` (Json.Number b) = a `compare` b
+    -- Below throws an error
+    compare a b = error $ "Field comparison not implemented: " ++ show (a,b)

src/Analyze/Check.hs
@@ -30,12 +30,12 @@ checkComparison
     :: [GroupName]
     -> Comparison
     -> [GroupName]
-checkComparison groupEnv (Comparison valueExpr _ _) =
+checkComparison groupEnv comparison =
     let groupNotFound name = "Grouping '" <> name <> "' doesn't exist"
         groupExists name = name `elem` groupEnv
         groupError name = if groupExists name then Nothing else Just (groupNotFound name)
-    in case valueExpr of
-        PosValueExpr (Get _) -> []
-        GroupValueExpr (CountDistinct _) -> []
-        GroupValueExpr (SumOver _ groupNameOpt) ->
+    in case comparison of
+        (GroupComparison (SumOver _ groupNameOpt) _ _) ->
             catMaybes [groupNameOpt >>= groupError]
+        _ -> []
+

src/Rules/CountryBondValue.hs
@@ -10,9 +10,9 @@ import Rules.Syntax
 
 ruleExpr :: RuleExpr
 ruleExpr =
-    where' (forall "InstrumentType") Eq "Bond" $:
+    where' (forall "InstrumentType" Eq "Bond") $:
         forEach "Country" $:
             forEach "IssuerName" $:
-                rule (sumOf value (relativeTo "Country")) LtE (Percent 5)
+                rule (sumOf value (relativeTo "Country") LtE (Percent 5))
 
 value = "DirtyValueTotalRC"

src/Rules/Syntax.hs
@@ -18,13 +18,18 @@ a +++ b = And a b
 infixr 7 +++
 
 forEach = GroupBy
-where' valExpr fComp value = Filter (Comparison valExpr fComp value)
-rule valExpr fComp value = Rule (Comparison valExpr fComp value)
+where' = Filter
+group = GroupComparison
+pos = PosComparison
+--(Comparison valExpr fComp value)
+rule = Rule
 sumOver = SumOver
 sumOverRelative fieldName relative = sumOver fieldName (Just relative)
 of' fieldName = sumOver fieldName (Just "Portfolio")
-numberOfRelativeTo = GroupValueExpr . CountDistinct
-numberOf = GroupValueExpr . CountDistinct
-forall = PosValueExpr . Get
-sumOf field rel = GroupValueExpr (SumOver field rel)
-relativeTo group = Just group
+numberOfRelativeTo = GroupComparison . CountDistinct
+numberOf field comp val = GroupComparison (CountDistinct field) comp val
+forall = PosComparison
+sumOf field rel comp val = GroupComparison (SumOver field rel) comp val
+relativeTo = Just
+
+-- sumOf' field rel = GroupComparison (SumOver field rel)

src/Rules/ThirtyfiveSixtySix.hs
@@ -18,11 +18,11 @@ import Rules.Syntax
 ruleExpr :: RuleExpr
 ruleExpr =
     forEach "IssuerName" $:
-        where' (sumOf value (relativeTo "Portfolio")) Gt (Percent 35) $:
-            rule (numberOf issue) GtE (Count 6)
+        where' (sumOf value (relativeTo "Portfolio") Gt (Percent 35)) $:
+            rule (numberOf issue GtE (Count 6))
             +++
             forEach issue |:
-                    rule (sumOf value (relativeTo "Portfolio")) LtE (Percent 30)
+                    rule (sumOf value (relativeTo "Portfolio") LtE (Percent 30))
 
 value = "DirtyValueTotalRC"
 issue = "SecurityID"

commit 62164709c4a42b109a4ac6cd971d35315e81a798

Fix bug introduced by baeec34

src/Eval/Eval.hs
@@ -106,7 +106,7 @@ evalComparisonGroup groupValueExpr fCompare expectedValue scopeData = do
   where
     currentLevelPositions = currentLevelPos scopeData
     groupCompare calculatedValue positions = do
-        if fCompare expectedValue calculatedValue
+        if fCompare calculatedValue expectedValue
             then ComparisonResult (Just positions) Nothing
             else ComparisonResult Nothing (Just positions)
 
@@ -118,7 +118,7 @@ evalComparisonPos
     -> EvalM (ComparisonResult Position)
 evalComparisonPos fieldName fCompare expectedValue currentLevelPositions = do
     valueFields <- lookupFields fieldName currentLevelPositions
-    return $ addManyResults (fCompare expectedValue) valueFields
+    return $ addManyResults (`fCompare` expectedValue) valueFields
   where
     addManyResults :: (FieldValue -> Bool) -> NonEmpty (Position, FieldValue) -> ComparisonResult Position
     addManyResults f =