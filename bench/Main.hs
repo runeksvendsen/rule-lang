2354e98437de4163a5229f2f18a75c0dbdc216ac
Rune K. Svendsen <runesvend@gmail.com>
2020-06-07 09:40:45 +0200

    Bench: evaluate environment

--- bench/Main.hs
+++ bench/Main.hs
@@ -8,7 +8,6 @@ import qualified Examples.Rules
 
 import qualified Criterion.Main as Crit
 import Test.QuickCheck (generate, vectorOf, arbitrary)
-import Data.List (group, sort)
 import Control.DeepSeq (force)
 import Control.Exception (evaluate)
 import System.IO (hFlush, stdout)
@@ -17,25 +16,22 @@ import System.IO (hFlush, stdout)
 main :: IO ()
 main = do
     logInfo "Generating test data ... "
-    posLists <- evaluate . force =<< mapM genPos counts
+    envList <- evaluate . force =<< mapM genEnv counts
     logInfo "done!\n"
     Crit.defaultMain
-      [ Crit.bgroup "Rule I"   $ map (mkBench Examples.Test.Pos.issuer Examples.Rules.ruleI) posLists
-      , Crit.bgroup "Rule Ia"  $ map (mkBench Examples.Test.Pos.issuer Examples.Rules.ruleIa) posLists
-      , Crit.bgroup "Rule II"  $ map (mkBench issuerIssue Examples.Rules.ruleII) posLists
-      , Crit.bgroup "Rule III" $ map (mkBench issuerIssue Examples.Rules.ruleIII) posLists
-      , Crit.bgroup "Rule IV"  $ map (mkBench Examples.Test.Pos.counterparty Examples.Rules.ruleIV) posLists
-      , Crit.bgroup "Rule V"   $ map (mkBench Examples.Test.Pos.securityID Examples.Rules.ruleV) posLists
-      , Crit.bgroup "Rule VI"  $ map (mkBench Examples.Test.Pos.country Examples.Rules.ruleVI) posLists
+      [ Crit.bgroup "Rule I"   $ map (mkBench Examples.Rules.ruleI) envList
+      , Crit.bgroup "Rule Ia"  $ map (mkBench Examples.Rules.ruleIa) envList
+      , Crit.bgroup "Rule II"  $ map (mkBench Examples.Rules.ruleII) envList
+      , Crit.bgroup "Rule III" $ map (mkBench Examples.Rules.ruleIII) envList
+      , Crit.bgroup "Rule IV"  $ map (mkBench Examples.Rules.ruleIV) envList
+      , Crit.bgroup "Rule V"   $ map (mkBench Examples.Rules.ruleV) envList
+      , Crit.bgroup "Rule VI"  $ map (mkBench Examples.Rules.ruleVI) envList
       ]
   where
     logInfo str = putStr str >> hFlush stdout
-    genPos :: Int -> IO [Examples.Test.Pos.TestPosition]
-    genPos count = generate (vectorOf count arbitrary)
-    counts = map (\n -> n * 100000) [1..10]
-    eval rule env = Eval.eval env rule
-    mkLabel f posList = show (countUnique f posList) <> "/" <> show (length posList)
+    counts = map (* 10000) [1..10]
+    mkBench rule (env, count) = Crit.bench (show count) $ Crit.nf (flip Eval.eval $ rule) env
     mkEnv posList = Eval.mkInitialEnv (map Examples.Test.Pos.toPos posList)
-    mkBench f rule (force -> !posList) = Crit.bench (mkLabel f posList) (Crit.nf (eval rule) (mkEnv posList))
-    countUnique f = length . group . sort . map f
-    issuerIssue p = (Examples.Test.Pos.issuer p, Examples.Test.Pos.issue p)
+    genEnv count = do
+        posList <- generate (vectorOf count arbitrary)
+        return (mkEnv posList, count)
