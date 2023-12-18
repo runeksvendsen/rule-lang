1698b09fedd93b877e83881193e5b9b31aba9106
Rune K. Svendsen <runesvend@gmail.com>
2023-12-18 12:07:51 +0100

    Make test suite fail on "eval" test failure
    It appears `quickCheck` was masking exceptions, thus not causing a non-zero exit code even though some of the "eval" tests were failing.
    
    TODO: make sure all test cases cause exit failure on test failure

--- test/Unit/Eval.hs
+++ test/Unit/Eval.hs
@@ -8,24 +8,29 @@ import Absyn
 import qualified Eval
 import qualified Examples.Rules
 import qualified Examples.Test.Pos
+import Test.Tasty
 
 import Test.QuickCheck
-import Control.Monad (forM_)
+import Data.Functor ((<&>))
+import Test.Tasty.QuickCheck  as QC
 
 
 main :: IO ()
-main =
-    forM_ positionCounts $ \posCount ->
-        forM_ Examples.Rules.allRules (testRule posCount)
+main = defaultMain $
+    testGroup "Eval: example rules" $ concat $
+        positionCounts <&> \posCount ->
+            Examples.Rules.allRules <&> \r@(_, name) -> 
+                QC.testProperty name $ testRule posCount r
   where
     positionCounts = [1, 10, 100, 1000, 10000]
 
-testRule :: Int -> (Rule, String) -> IO ()
+testRule :: Int -> (Rule, String) -> Gen Property
 testRule numPositions (rule, name) = do
-    testPositions <- generate $ vectorOf numPositions arbitrary
+    testPositions <- vectorOf numPositions arbitrary
     let positions = map Examples.Test.Pos.toPos testPositions
     let lbl = "Rule " <> name <> ": (" <> show numPositions <> ")"
     let passed = Eval.eval (Eval.mkInitialEnv positions) rule
     -- We're not interested in whether an arbitrary set of positions passes a rule,
     --  rather, we just want to catch runtime errors in the evaluator.
-    quickCheck $ label lbl $ passed || True
+    let passed' = passed || True
+    pure $ property $ label lbl passed'
